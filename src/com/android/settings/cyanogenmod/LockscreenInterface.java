059c95136f6b31a04975d7e0e5beaabaec6e4338
cr3pt <cr3pt@o2.pl>
2014-01-28 18:27:59 +0100

    Revert "Move lockscreen buttons options to one place"
    This reverts commit 2bb891c4d3aa5f5fbf56550192d5e41339434623.

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -81,8 +81,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         }
 
         // Remove lockscreen button actions if device doesn't have hardware keys
-        // and doesn't have a hardware camera key
-        if (!hasButtons() && !LockscreenButtons.hasCameraKey) {
+        if (!hasButtons()) {
             generalCategory.removePreference(findPreference(KEY_LOCKSCREEN_BUTTONS));
         }
 
